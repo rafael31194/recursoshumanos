59469f91e07e23d6345d0d1d2f25cf78a249d5ea
jorgeaguirreH <jorgeaguirre1994@hotmail.com>
2015-08-22 13:57:31 -0600

    CREACIONES DE BEL PARA NUEVAS TABLAS- ALBERTO

---
+++ HelpDesk.RecursosHumanos.BEL/EstadoProyectoE.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDesk.RecursosHumanos.BEL
+{
+    public class EstadoProyectoE
+    {
+        public int id_estadoProyecto { get; set; }
+        public string descripcion_estadoProyecto { get; set; }
+
+        public EstadoProyectoE() { }
+        public EstadoProyectoE(int pId_estadoProyecto, string pDescripcion_estadoProyecto)
+        {
+            id_estadoProyecto = pId_estadoProyecto;
+            descripcion_estadoProyecto = pDescripcion_estadoProyecto;
+        }
+    }
+}
---
+++ HelpDesk.RecursosHumanos.BEL/PaisE.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDesk.RecursosHumanos.BEL
+{
+    public class PaisE
+    {
+        public int id_pais { get; set; }
+        public string nombre_pais { get; set; }
+        public int id_moneda { get; set; }
+
+        public PaisE() { }
+        public PaisE(int pId_pais, string pNombre_pais, int pId_moneda)
+        {
+            id_pais = pId_pais;
+            nombre_pais = pNombre_pais;
+            id_moneda = pId_moneda;
+
+        }
+    }
+}
