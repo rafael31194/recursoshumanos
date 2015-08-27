8a77f231749623e419ec94ced8c0ac42f0da023c
rafael31194 <rafael31194@hotmail.com>
2015-08-26 19:34:17 -0500

    completado lo de Proyectos-Eduardo

---
+++ HelpDesk.RecursosHumanos.BLL/ProyectoBLL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelpDesk.RecursosHumanos.DAL;
+
+namespace HelpDesk.RecursosHumanos.BEL
+{
+    public  class ProyectoBLL
+    {
+
+        ProyectoDAL _proyecto = new ProyectoDAL();
+        public ProyectoBLL()
+        {
+
+        }
+
+
+        public int agregarProyecto(ProyectoE proyecto, int id,ref string oerro)
+        {
+            return _proyecto.AgregarProyecto(proyecto, id, ref oerro);
+        }
+
+        public int actualizarProyecto(ProyectoE pro, ref string oerro)
+        {
+            try{
+                return _proyecto.actualizarProyecto(pro, ref oerro);
+
+            }
+            catch{
+                return 0;
+                }
+        }
+
+
+        public void eliminarProyecto(int idProyecDelete, int idEmpleado,ref string oerro)
+        {
+            _proyecto.eliminarProyecto(idProyecDelete,idEmpleado, ref oerro);
+        }
+    }
+}
---
+++ HelpDesk.RecursosHumanos.DAL/ProyectoEmpleadoDAL.cs
@@ -0,0 +1,48 @@
+﻿using HelpDesk.RecursosHumanos.BEL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelpDesk.RecursosHumanos.DAL
+{
+    public class ProyectoEmpleadoDAL
+    {
+
+        public ProyectoEmpleadoDAL()
+        {
+
+        }
+
+
+        public int InsertarProyectoEmpleado(ProyectoEmpleadosE proyecto, ref string oerro)
+        {
+            try
+            {
+                using (SqlConnection _conn = CommonDb.ObtenerConnSql())
+                {
+                    SqlConnection oConn = CommonDb.ObtenerConnSql();
+                    SqlCommand oCmd = new SqlCommand("SP_insertar_ProyectoEmpleado", oConn);
+                    oCmd.Parameters.AddWithValue("@idEmpleado", proyecto.id_empleado);
+                    oCmd.Parameters.AddWithValue("@idProyecto", proyecto.id_proyectos);
+                    oCmd.CommandType = CommandType.StoredProcedure;
+                    return oCmd.ExecuteNonQuery();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrio un error al insertar el proyecto del empleado");
+                return 0;
+                throw (ex);
+                
+
+            }
+
+        }
+    }
+}
