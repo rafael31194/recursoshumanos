7900e4977000b821d1cbe38785fedf6a44dbf21c
jorgeaguirreH <jorgeaguirre1994@hotmail.com>
2015-09-07 16:52:31 -0600

    Cree una clase para llamar el metodo "enviarCorreo" la clase se llama (sendEmail) y esta en la carpeta utilerias de la capa de presentacion, le puse mensaje de confirmacion cuando le de enviar el correo  (se tarda unos segundos en hacer ese proceso) edite los reportes para que no hagan salto de pagina .

--- WpfApplication3/CurriculumEmpleado.xaml.cs
+++ WpfApplication3/CurriculumEmpleado.xaml.cs
@@ -158,68 +158,68 @@ namespace WpfApplication3
         {
             NuevoCorreo nuevoEmail = new NuevoCorreo(this);
             nuevoEmail.Show();
-            
+            this.Close();
         }
 
 
 
-        internal void EnviarDatosCorreo(string destinatario, string subject, string message)
-        {
-            try
-            {
-                Warning[] warnings;
-                string[] streamids;
-                string mimeType;
-                string encoding;
-                string extension;
+        //internal void EnviarDatosCorreo(string destinatario, string subject, string message)
+        //{
+        //    try
+        //    {
+        //        Warning[] warnings;
+        //        string[] streamids;
+        //        string mimeType;
+        //        string encoding;
+        //        string extension;
 
-                byte[] bytes = ReportViewerCurriculumEmp.LocalReport.Render(
-                   "PDF", null, out mimeType, out encoding,
-                    out extension,
-                   out streamids, out warnings);
-
-
-                // add the attachment from a stream
-                System.IO.MemoryStream memStream = new System.IO.MemoryStream(bytes);
-
-                System.IO.StreamWriter streamWriter = new System.IO.StreamWriter(memStream);
-                streamWriter.Flush();
-
-                // this is quite important
-                memStream.Position = 0;
-
-                System.Net.Mail.Attachment thisAttachment = new System.Net.Mail.Attachment(memStream, "aplication/pdf");
-                thisAttachment.ContentDisposition.FileName = "Curriculum.pdf";
+        //        byte[] bytes = ReportViewerCurriculumEmp.LocalReport.Render(
+        //           "PDF", null, out mimeType, out encoding,
+        //            out extension,
+        //           out streamids, out warnings);
 
 
+        //        // add the attachment from a stream
+        //        System.IO.MemoryStream memStream = new System.IO.MemoryStream(bytes);
+
+        //        System.IO.StreamWriter streamWriter = new System.IO.StreamWriter(memStream);
+        //        streamWriter.Flush();
+
+        //        // this is quite important
+        //        memStream.Position = 0;
+
+        //        System.Net.Mail.Attachment thisAttachment = new System.Net.Mail.Attachment(memStream, "aplication/pdf");
+        //        thisAttachment.ContentDisposition.FileName = "Curriculum.pdf";
 
 
-                //Configurando el cliente SMTP
-                SmtpClient client = new SmtpClient();
-                client.Host = "smtp.gmail.com";
-                client.Port = 587;
-                client.EnableSsl = true;
-                client.DeliveryMethod = SmtpDeliveryMethod.Network;
-                client.UseDefaultCredentials = false;
-                client.Credentials = new NetworkCredential("helpdesk.enviar@gmail.com", "contraenviar");
-                //Preparando archivo adjunto
 
 
-                //Enviando correo
-                MailMessage mail = new MailMessage();
-                mail.From = new MailAddress("helpdesk.enviar@gmail.com");
-                mail.To.Add(new MailAddress(destinatario));
-                mail.Subject = subject;
-                mail.IsBodyHtml = false;
-                mail.Body = message;
-                mail.Attachments.Add(thisAttachment);
-                client.Send(mail);
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show("Ocurrio un error al adjuntar y enviar el correo");
-            }
-        }
+        //        //Configurando el cliente SMTP
+        //        SmtpClient client = new SmtpClient();
+        //        client.Host = "smtp.gmail.com";
+        //        client.Port = 587;
+        //        client.EnableSsl = true;
+        //        client.DeliveryMethod = SmtpDeliveryMethod.Network;
+        //        client.UseDefaultCredentials = false;
+        //        client.Credentials = new NetworkCredential("helpdesk.enviar@gmail.com", "contraenviar");
+        //        //Preparando archivo adjunto
+
+
+        //        //Enviando correo
+        //        MailMessage mail = new MailMessage();
+        //        mail.From = new MailAddress("helpdesk.enviar@gmail.com");
+        //        mail.To.Add(new MailAddress(destinatario));
+        //        mail.Subject = subject;
+        //        mail.IsBodyHtml = false;
+        //        mail.Body = message;
+        //        mail.Attachments.Add(thisAttachment);
+        //        client.Send(mail);
+        //    }
+        //    catch(Exception ex)
+        //    {
+        //        MessageBox.Show("Ocurrio un error al adjuntar y enviar el correo");
+        //    }
+        //}
 
         //protected void GenerarEnviarPdf(int idOrden)
         //{
---
+++ WpfApplication3/Utilerias/sendEmail.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication3.Utilerias
+{
+   public class sendEmail
+    {
+       public bool EnviarDatosCorreo(string destinatario, string subject, string message, ReportViewer ReportControl, ref string error)
+       {
+           bool result = false;
+           try
+           {
+               Warning[] warnings;
+               string[] streamids;
+               string mimeType;
+               string encoding;
+               string extension;
+
+               byte[] bytes = ReportControl.LocalReport.Render(
+                  "PDF", null, out mimeType, out encoding,
+                   out extension,
+                  out streamids, out warnings);
+
+
+               // add the attachment from a stream
+               System.IO.MemoryStream memStream = new System.IO.MemoryStream(bytes);
+
+               System.IO.StreamWriter streamWriter = new System.IO.StreamWriter(memStream);
+               streamWriter.Flush();
+
+               // this is quite important
+               memStream.Position = 0;
+
+               System.Net.Mail.Attachment thisAttachment = new System.Net.Mail.Attachment(memStream, "aplication/pdf");
+               thisAttachment.ContentDisposition.FileName = "Curriculum.pdf";
+
+
+
+
+               //Configurando el cliente SMTP
+               SmtpClient client = new SmtpClient();
+               client.Host = "smtp.gmail.com";
+               client.Port = 587;
+               client.EnableSsl = true;
+               client.DeliveryMethod = SmtpDeliveryMethod.Network;
+               client.UseDefaultCredentials = false;
+               client.Credentials = new NetworkCredential("helpdesk.enviar@gmail.com", "contraenviar");
+               //Preparando archivo adjunto
+
+
+               //Enviando correo
+               MailMessage mail = new MailMessage();
+               mail.From = new MailAddress("helpdesk.enviar@gmail.com");
+               mail.To.Add(new MailAddress(destinatario));
+               mail.Subject = subject;
+               mail.IsBodyHtml = false;
+               mail.Body = message;
+               mail.Attachments.Add(thisAttachment);
+               client.Send(mail);
+
+               result = true;
+           }
+           catch (Exception ex)
+           {
+               result = false;
+               error = ex.Message;
+           }
+           return result;
+       }
+    }
+}
